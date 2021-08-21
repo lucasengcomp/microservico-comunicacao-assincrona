ec06026e2efcb00c1e1449c3308776d75222bb4b
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2021-08-20 22:33:43 -0300

    Project message with RabbitMQ

---
+++ src/main/java/com/ms/email/MsComunicacaoAssincronaApplication.java
@@ -0,0 +1,13 @@
+package com.ms.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsComunicacaoAssincronaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsComunicacaoAssincronaApplication.class, args);
+    }
+
+}
